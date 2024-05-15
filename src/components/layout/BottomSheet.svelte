e323ffdd391c41a0fc69a45e6d87226c86862d08
Tormak <63308171+Tormak9970@users.noreply.github.com>
2024-05-15 10:01:09 -0500

    chore: back to where we were

--- src/components/layout/BottomSheet.svelte
+++ src/components/layout/BottomSheet.svelte
@@ -1,48 +1,73 @@
 <script lang="ts">
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
   import type { TransitionConfig } from "svelte/transition";
   import { easeEmphasizedAccel, easeEmphasizedDecel } from "../animations/easing";
   import { outroClass } from "../animations/animations";
-
-  let height = 480;
-  let container: HTMLDivElement;
-  let isDragging = false,
-    startY = 0;
-  $: if (height < 48) dispatch("close", "low");
-
-  const dispatch = createEventDispatcher();
-  const open = (node: HTMLDialogElement) => node.showModal();
-  const heightAnim = (
-    node: HTMLDialogElement,
-    options: Record<string, unknown> = {},
-  ): TransitionConfig => {
+  
+  /**
+   * The bottom sheet height animation.
+   */
+  function heightAnim(node: HTMLDialogElement, options: Record<string, unknown> = {}): TransitionConfig {
     if (node.clientHeight < height) height = node.clientHeight;
+
     return {
       duration: 400,
       easing: easeEmphasizedDecel,
       ...options,
       css: (t) => `max-height: ${t * height}px`,
-    };
-  };
+    }
+  }
 
-  const moveWheel = (e: WheelEvent) => {
-    height += e.deltaY;
-    if (container && container.clientHeight < height) height = container.clientHeight;
-  };
-  const moveMouse = (e: { clientY: number }) => {
+  let dialogElement: HTMLDialogElement;
+
+  let fullHeight: number;
+  export let height = 1000;
+  
+  let isDragging = false;
+  let startY = 0;
+
+  const dispatch = createEventDispatcher();
+  const open = (node: HTMLDialogElement) => {
+    node.inert = true;
+    node.showModal();
+    node.inert = false;
+  }
+
+  function moveMouse(e: { clientY: number }) {
     if (isDragging) {
       const distance = e.clientY - startY;
       height -= distance;
+      if (height > fullHeight) height = fullHeight;
       startY = e.clientY;
     }
-  };
+  }
+
+  function onDragStart(y: number) {
+    isDragging = true;
+    startY = y;
+  }
+
+  function onDragEnd() {
+    isDragging = false;
+
+    if (height < fullHeight / 2) {
+      dispatch("close", "low");
+    } else {
+      height = fullHeight;
+    }
+  }
+
+  onMount(() => {
+    fullHeight = dialogElement.scrollHeight;
+    height = fullHeight;
+  });
 </script>
 
 <svelte:window
   on:mousemove={moveMouse}
-  on:mouseup={() => (isDragging = false)}
+  on:mouseup={onDragEnd}
   on:touchmove={(e) => moveMouse(e.touches[0])}
-  on:touchend={() => (isDragging = false)}
+  on:touchend={onDragEnd}
 />
 
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
@@ -57,25 +82,18 @@
   on:mousedown|self={() => {
     dispatch("close", "click");
   }}
-  on:wheel|preventDefault={moveWheel}
   in:heightAnim
   out:heightAnim={{ easing: easeEmphasizedAccel, duration: 300 }}
+  bind:this={dialogElement}
 >
   <div
     class="container"
-    bind:this={container}
-    on:touchstart={(e) => {
-      isDragging = true;
-      startY = e.touches[0].clientY;
-    }}
+    on:touchstart={(e) => onDragStart(e.touches[0].clientY)}
   >
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <div
       class="handle-container"
-      on:mousedown|preventDefault={(e) => {
-        isDragging = true;
-        startY = e.clientY;
-      }}
+      on:mousedown|preventDefault={(e) => onDragStart(e.clientY)}
     >
       <div class="handle" />
     </div>
@@ -85,7 +103,7 @@
 
 <style>
   :root {
-    --m3-bottom-sheet-shape: var(--m3-util-rounding-extra-large);
+    --m3-bottom-sheet-shape: var(--m3-util-rounding-large);
   }
 
   .m3-container {
--- src/components/overlays/options/SongOptions.svelte
+++ src/components/overlays/options/SongOptions.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
+  import { Divider, Icon, ListItemButton } from "m3-svelte";
   import { AppController } from "../../../lib/controllers/AppController";
   import { PlaybackController } from "../../../lib/controllers/PlaybackController";
   import { QueueController } from "../../../lib/controllers/QueueController";
-  import type { Song } from "../../../lib/models/Song";
   import { showAddToPlaylist, showSongDetails, songToAdd, albumViewing, artistViewing, songViewing, showEditSong, showAlbumDetails, showSongOptions } from "../../../stores/Overlays";
   import { songsMap } from "../../../stores/State";
   import BottomSheet from "../../layout/BottomSheet.svelte";
+  import Play from "@ktibow/iconset-material-symbols/play-arrow-rounded";
+  import AddBox from "@ktibow/iconset-material-symbols/add-box-rounded";
+  import PlaylistAdd from "@ktibow/iconset-material-symbols/playlist-add-rounded";
+  import Album from "@ktibow/iconset-material-symbols/album";
+  import PlayNext from "@ktibow/iconset-material-symbols/resume-rounded";
+  import Info from "@ktibow/iconset-material-symbols/info";
+  import Artist from "@ktibow/iconset-material-symbols/artist-rounded";
+  import Edit from "@ktibow/iconset-material-symbols/edit-rounded";
+  import Share from "@ktibow/iconset-material-symbols/share";
+  import Trash from "@ktibow/iconset-material-symbols/delete-forever-rounded";
+  import type { Song } from "../../../lib/models/Song";
 
-  $: song = $songViewing ? $songsMap[$songViewing] : null;
+  $: song = ($songViewing ? $songsMap[$songViewing] : null) as Song;
 
   function closeOptions() {
     $songViewing = null;
@@ -51,41 +62,78 @@
 
 {#if $showSongOptions}
   <BottomSheet on:close={closeOptions}>
-    This is a test!
+    <div class="list">
+      <ListItemButton headline="Play" on:click={() => PlaybackController.playSong(song)}>
+        <svelte:fragment slot="leading">
+          <Icon icon={Play} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Play Next" on:click={() => QueueController.playNext([song.title])}>
+        <svelte:fragment slot="leading">
+          <Icon icon={PlayNext} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Add to Queue" on:click={() => QueueController.queueSongs([song.title])}>
+        <svelte:fragment slot="leading">
+          <Icon icon={AddBox} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Add to Playlist" on:click={addToPlaylist}>
+        <svelte:fragment slot="leading">
+          <Icon icon={PlaylistAdd} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Go to Album" on:click={goToAlbum}>
+        <svelte:fragment slot="leading">
+          <Icon icon={Album} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      {#if song?.artist}
+        <ListItemButton headline="Go to Artist" on:click={goToArtist}>
+          <svelte:fragment slot="leading">
+            <Icon icon={Artist} />
+          </svelte:fragment>
+        </ListItemButton>
+        <Divider />
+      {/if}
+      <ListItemButton headline="Details" on:click={showDetails}>
+        <svelte:fragment slot="leading">
+          <Icon icon={Info} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Edit" on:click={showSongEdit}>
+        <svelte:fragment slot="leading">
+          <Icon icon={Edit} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Share" on:click={() => AppController.share([song.title])}>
+        <svelte:fragment slot="leading">
+          <Icon icon={Share} />
+        </svelte:fragment>
+      </ListItemButton>
+      <Divider />
+      <ListItemButton headline="Delete" on:click={() => AppController.deleteFromDevice([song.title])}>
+        <svelte:fragment slot="leading">
+          <Icon icon={Trash} />
+        </svelte:fragment>
+      </ListItemButton>
+    </div>
+    <div style="width: 100%; height: 20px;" />
   </BottomSheet>
 {/if}
 
-<!-- <md-menu-item style="width: 180px;" on:click={() => PlaybackController.playSong(song)}>
-  <div slot="headline">Play</div>
-</md-menu-item>
-<md-menu-item on:click={() => QueueController.playNext([song.title])}>
-  <div slot="headline">Play Next</div>
-</md-menu-item>
-<md-menu-item on:click={() => QueueController.queueSongs([song.title])}>
-  <div slot="headline">Add to queue</div>
-</md-menu-item>
-<md-menu-item on:click={addToPlaylist}>
-  <div slot="headline">Add to playlist</div>
-</md-menu-item>
-{#if song.album}
-  <md-menu-item on:click={goToAlbum}>
-    <div slot="headline">Go to album</div>
-  </md-menu-item>
-{/if}
-{#if song.artist}
-  <md-menu-item on:click={goToArtist}>
-    <div slot="headline">Go to artist</div>
-  </md-menu-item>
-{/if}
-<md-menu-item on:click={showDetails}>
-  <div slot="headline">Details</div>
-</md-menu-item>
-<md-menu-item on:click={showSongEdit}>
-  <div slot="headline">Edit</div>
-</md-menu-item>
-<md-menu-item on:click={() => AppController.share([song.title])}>
-  <div slot="headline">Share</div>
-</md-menu-item>
-<md-menu-item on:click={() => AppController.deleteFromDevice([song.title])}>
-  <div slot="headline">Delete from device</div>
-</md-menu-item> -->
+<style>
+  .list {
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    overflow: hidden;
+  }
+</style>
